7f65537f430f007a994746336d9399b9e3c6bb55
Oussama-Ben-Nasr <oussama.bennasr@supcom.tn>
2023-11-06 21:25:29 +0100

    fix: color state mapping

--- cube_env/render/render.py
+++ cube_env/render/render.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 color = {
     0: "red",
     1: "orange",
@@ -7,9 +10,16 @@ color = {
     5: "yellow"
 }
 
+def cell_id_to_state_x_y(x: int) -> Tuple[int, int]:
+    q: int = x//9
+    r: int = x%9
+    _x: int = r//3 + 3*(q//3)
+    _y: int = r%3 + 3*(q%3)
+    return (_x, _y)
+
 def render(cube_env_inst):
     with open("cube_env/render/color_cube.js", "w") as f:
         for cell_id in range(54):
-            f.write(f"document.getElementById(\"cell{cell_id}\").style.backgroundColor = \"{color[cube_env_inst._state[cell_id // 9][cell_id % 9]]}\";\n")
+            f.write(f"document.getElementById(\"cell{cell_id}\").style.backgroundColor = \"{color[cube_env_inst._state[cell_id_to_state_x_y(cell_id)[0]][cell_id_to_state_x_y(cell_id)[1]]]}\";\n")
             if(cell_id == 53):
                 f.write("exit();")
