adaa48253397d1fd23cd3a6baa7df533b4821160
Oussama-Ben-Nasr <oussama.bennasr@supcom.tn>
2023-11-21 22:23:04 +0100

    code: add heap DS

---
+++ cube_env/helpers/heap.py
@@ -0,0 +1,158 @@
+from abc import ABC, abstractmethod
+
+class HeapStrategy(ABC):
+    @abstractmethod
+    def heap_sift_down_order(self, first: int, second) -> bool:
+        pass
+    
+    @abstractmethod
+    def heap_sift_up_order(self, first: int, second) -> bool:
+        pass
+
+class MaxHeapStrategy(HeapStrategy):
+    def heap_sift_down_order(self, first: int, second) -> bool:
+        return first >= second
+    
+    def heap_sift_up_order(self, first: int, second) -> bool:
+        return first < second
+
+class MinHeapStrategy(HeapStrategy):
+    def heap_sift_down_order(self, first: int, second) -> bool:
+        return first <= second
+    
+    def heap_sift_up_order(self, first: int, second) -> bool:
+        return first > second
+
+class Heap(ABC):
+    """Heap commun implementation."""
+    mem: list
+    def __init__(self) -> None:
+        self.mem = []
+
+    def peek(self) -> int:
+        if self.empty:
+            raise Exception("Peek on empty heap.")
+        return self.mem[0]
+    
+    @property
+    def size(self) -> int:
+        """return the number of items in the heap."""
+        return len(self.mem)
+    
+    @property
+    def empty(self) -> bool:
+        return 0 == len(self.mem)
+    
+
+    def valid_index(self, index) -> bool:
+        return index >= 0 and index < self.size
+    
+
+    def has_parent(self, index) -> bool:
+        return self.parent_index(index) >= 0
+    
+    def insert(self, key: int, order: HeapStrategy) -> None:
+        self.mem.append(key)
+        self.sift_up(self.size-1, order)
+    
+    def parent_index(self, index: int) -> int:
+        """Return the parent index for a given node."""
+        if self.size <= index:
+            raise Exception(f"Index {index} greater than the size of the heap {self.size}")
+        return (index - 1) // 2
+    
+    def right_child_index(self, index: int) -> int:
+        """Return the right child index for a given node."""
+        if self.size <= index:
+            raise Exception(f"Index {index} greater than the size of the heap {self.size}")
+        return 2*index + 2
+    
+    def left_child_index(self, index: int) -> int:
+        """Return the left child index for a given node."""
+        if self.size <= index:
+            raise Exception(f"Index {index} greater than the size of the heap {self.size}")
+        return 2*index + 1
+    
+    def swap_index(self, index: int, other: int) -> None:
+        """Swap two nodes given their indexes."""
+        self.mem[index], self.mem[other] = self.mem[other], self.mem[index]
+
+    def sift_up(self, index, order: HeapStrategy) -> None:
+        """
+        Move a node up in the tree, as long as needed; used to restore heap condition after insertion. 
+        Called "sift" because node moves up the tree until it reaches the correct level, as in a sieve.
+            1. if I have a parent smaller than me swap and go up
+        """
+        val = self.mem[index]
+        while self.has_parent(index) and order.heap_sift_up_order(self.mem[self.parent_index(index)], val):
+            self.swap_index(index, self.parent_index(index))
+            index = self.parent_index(index)
+
+    def sift_down(self, index, order: HeapStrategy) -> None:
+        """
+        Move a node down in the tree, similar to sift-up; used to restore heap condition after deletion or replacement.
+            1. if has no chilren then return
+            2. has left child only then swap if this child id greater than me
+            3. has left and right children then swap with the biggest child
+        """
+        if self.empty:
+            return
+        val = self.mem[index]
+        while self.valid_index(self.left_child_index(index)) or self.valid_index(self.right_child_index(index)):
+            if self.valid_index(self.left_child_index(index)) and self.valid_index(self.right_child_index(index)):
+                if order.heap_sift_down_order(val, self.mem[self.left_child_index(index)]) and order.heap_sift_down_order(val,  self.mem[self.right_child_index(index)]):
+                    return
+                left_val = self.mem[self.left_child_index(index)]
+                right_val = self.mem[self.right_child_index(index)]
+                if order.heap_sift_down_order(left_val, right_val):
+                    self.swap_index(index, self.left_child_index(index))
+                    index = self.left_child_index(index)
+                else:
+                    self.swap_index(index, self.right_child_index(index))
+                    index = self.right_child_index(index)
+            else:
+                if order.heap_sift_down_order(val, self.mem[self.left_child_index(index)]):
+                    return
+                self.swap_index(index, self.left_child_index(index))
+                index = self.left_child_index(index)
+
+class MaxHeap(Heap):
+    def __init__(self, container: list = []) -> None:
+        super().__init__()
+        for key in container:
+            self.insert(key)
+        
+    def insert(self, key: int, order: HeapStrategy= MaxHeapStrategy()) -> None:
+        return super().insert(key, order)
+
+    def extract_max(self) -> int:
+        self.swap_index(0, self.size-1)
+        max = self.mem.pop()
+        self.sift_down(0, MaxHeapStrategy())
+        return max
+    
+    def delete_max(self) -> None:
+        self.swap_index(0, self.size-1)
+        self.mem.pop()
+        self.sift_down(0, MaxHeapStrategy())
+
+class MinHeap(Heap):
+    def __init__(self, container: list = []) -> None:
+        super().__init__()
+        for key in container:
+            self.insert(key)
+    
+    def insert(self, key: int, order: HeapStrategy= MinHeapStrategy()) -> None:
+        return super().insert(key, order)
+
+
+    def extract_min(self) -> int:
+        self.swap_index(0, self.size-1)
+        max = self.mem.pop()
+        self.sift_down(0, MinHeapStrategy())
+        return max
+    
+    def delete_min(self) -> None:
+        self.swap_index(0, self.size-1)
+        self.mem.pop()
+        self.sift_down(0, MinHeapStrategy())
---
+++ test/test_heap.py
@@ -0,0 +1,84 @@
+from cube_env.helpers.heap import MaxHeap, MinHeap, MaxHeapStrategy, MinHeapStrategy
+
+def test_create_max_heap() -> None:
+    l = [5, 17, 10, 1, 2, 5, 19]
+    max_heap = MaxHeap(l)
+    assert max_heap.size == len(l)
+
+
+def test_insert_max_heap() -> None:
+    l = [5, 17, 10, 1, 2, 5, 19]
+    max_heap = MaxHeap()
+    for elem in l:
+        max_heap.insert(elem)
+    assert max_heap.size == len(l)
+
+
+def test_stif_down_max_heap() -> None:
+    l = [5, 17, 10, 1, 2, 5, 19]
+    max_heap = MaxHeap(l)
+    while max_heap.size > 2:
+        max_heap.extract_max()
+    max_heap.sift_down(0, MaxHeapStrategy())
+    assert max_heap.mem == [2, 1]
+
+
+def test_delete_max_in_max_heap_in_right_order() -> None:
+    l = [5, 17, 10, 1, 2, 5, 19]
+    initial_size, deleted_elems = len(l), 0
+    max_heap = MaxHeap(l)
+    while not max_heap.empty:
+        assert max_heap.peek() == list(reversed(sorted(l)))[deleted_elems]
+        max_heap.delete_max()
+        deleted_elems += 1
+        assert max_heap.size == initial_size-deleted_elems
+    assert max_heap.mem == []
+
+
+def test_max_heap_can_extract_max_in_sorted_order() -> None:
+    l = [5, 17, 10, 1, 2, 5, 19]
+    max_heap = MaxHeap(l)
+    for elem in reversed(sorted(l)):
+        assert elem == max_heap.extract_max()
+
+
+def test_create_min_heap() -> None:
+    l = [5, 17, 10, 1, 2, 5, 19]
+    min_heap = MinHeap(l)
+    assert min_heap.size == len(l)
+
+
+def test_insert_min_heap() -> None:
+    l = [5, 17, 10, 1, 2, 5, 19]
+    min_heap = MinHeap()
+    for elem in l:
+        min_heap.insert(elem)
+    assert min_heap.size == len(l)
+
+
+def test_stif_down_min_heap() -> None:
+    l = [5, 17, 10, 1, 2, 5, 19]
+    min_heap = MinHeap(l)
+    while min_heap.size > 2:
+        min_heap.extract_min()
+    min_heap.sift_down(0, MinHeapStrategy())
+    assert min_heap.mem == [17, 19]
+
+
+def test_delete_min_in_min_heap_in_right_order() -> None:
+    l = [5, 17, 10, 1, 2, 5, 19]
+    initial_size, deleted_elems = len(l), 0
+    min_heap = MinHeap(l)
+    while not min_heap.empty:
+        assert min_heap.peek() == list(sorted(l))[deleted_elems]
+        min_heap.delete_min()
+        deleted_elems += 1
+        assert min_heap.size == initial_size-deleted_elems
+    assert min_heap.mem == []
+
+
+def test_min_heap_can_extract_max_in_sorted_order() -> None:
+    l = [5, 17, 10, 1, 2, 5, 19]
+    min_heap = MinHeap(l)
+    for elem in sorted(l):
+        assert elem == min_heap.extract_min()
